4f369bbfa6c683b5f080eb7b9e991cd54d45dc26
Abhrasnata Ray <abhrasnataray@gmail.com>
2025-06-29 02:44:00 +0530

    feat: Completed some part of Login Page

--- frontend/src/pages/Auth/Login.jsx
+++ frontend/src/pages/Auth/Login.jsx
@@ -1,6 +1,7 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 import AuthLayout from "../../components/layouts/AuthLayout"
-import { useNavigate } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
+import Input from "../../components/Inputs/Input"
 
 const Login = () => {
 
@@ -9,6 +10,26 @@ const Login = () => {
   const [error, setError] = useState(null)
 
   const navigate = useNavigate();
+
+  // Handle Login Form Submit
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+
+    if(!validateEmail(email)){
+      setError("Please enter a valid email address")
+      return;
+    }
+
+    if(!password){
+      setError("Please enter the password")
+      return;
+    }
+
+    setError("")
+    // Login API call
+
+  }
 
   return (
     <AuthLayout>
@@ -22,13 +43,38 @@ const Login = () => {
 
 
         <form onSubmit={handleLogin}>
-          <input
+
+          <Input
             value={email}
-            onChange={({target}) => setEmail(target.value)}
+            onChange={({ target }) => setEmail(target.value)}
             label="Email Address"
             placeholder='john@example.com'
-            type='text'/>
+            type='text' />
+
+          <Input
+            value={password}
+            onChange={({ target }) => setPassword(target.value)}
+            label="Password"
+            placeholder='Min 8 Characters'
+            type='password' />
+
+          {error && <p className='text-red-500 text-xs pb-2.5'>{error}</p>}
+
+          <button type='submit' className='btn-primary'>LOGIN</button>
+
+          <p className='text-[13px] text-slate-800 mt-3'>
+
+            Don't have an account?{" "}
+
+            <Link className='font-medium text-primary underline' to="/signup">
+              Sign Up
+            </Link>
+
+          </p>
+
         </form>
+
+
 
       </div>
     </AuthLayout>
